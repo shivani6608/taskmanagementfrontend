eecde9ec85a23fdb50b2a020dd6870299d28a8c9
shivani6608 <155462928+shivani6608@users.noreply.github.com>
2026-08-20 23:17:27 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CheckSquare, LogOut } from 'lucide-react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/authContext';
 export const Navbar = () => {
  const { user, logout } = useAuth();
  const navigate = useNavigate();
