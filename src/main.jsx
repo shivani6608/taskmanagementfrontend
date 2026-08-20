70a265f15df8d3c884c856af8054192cfa1a517d
shivani6608 <155462928+shivani6608@users.noreply.github.com>
2026-08-20 22:53:38 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from 'src/context/AuthContext.jsx';
 import './index.css';
 const queryClient = new QueryClient({
  defaultOptions: {
