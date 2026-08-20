d2d08be8ebcf7d16545c8fa05ab54a9d8c09cbcd
shivani6608 <155462928+shivani6608@users.noreply.github.com>
2026-08-20 23:19:09 +0530

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context/authContext';
 export const RegisterPage = () => {
  const { register, loading } = useAuth();
  const navigate = useNavigate();
